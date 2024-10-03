a81618f97bb1299efadce3e098c72e0ba6709e99
aldum <aldum@artixlinux.org>
2024-10-03 16:21:29 +0200

    fix(editor): handle replacing with Empty

--- src/model/editor/bufferModel.lua
+++ src/model/editor/bufferModel.lua
@@ -209,20 +209,27 @@ function BufferModel:replace_selected_text(t)
       end
     end)()
 
-    --- remove old chunk
-    self.content:remove(sel)
-    --- insert new version of the chunk(s)
-    for i = #chunks, 1, -1 do
-      local c = chunks[i]
-      local nr = c.pos:translate(cs - 1)
-      c.pos = nr
-      self.content:insert(c, sel)
+    Log.debug(cs, ol, '\n', Debug.terse_array(self.content, sel))
+    if n == 1 then
+      self.content[sel] = chunks[1]
+    else
+      --- remove old chunk
+      self.content:remove(sel)
+      --- insert new version of the chunk(s)
+      for i = #chunks, 1, -1 do
+        local c = chunks[i]
+        local nr = c.pos:translate(cs - 1)
+        c.pos = nr
+        self.content:insert(c, sel)
+      end
     end
     --- move subsequent chunks down
     local diff = chunks[n].pos:len() - ol
-    for i = sel + 1, self:get_content_length() do
-      local b = self.content[i]
-      b.pos = b.pos:translate(diff)
+    if diff ~= 0 then
+      for i = sel + 1, self:get_content_length() do
+        local b = self.content[i]
+        b.pos = b.pos:translate(diff)
+      end
     end
 
     return true, n
