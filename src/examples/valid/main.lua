96ae485f5d175d7ff47fbe8d9ce0f1d01ce6d86c
aldum <aldum@artixlinux.org>
2024-12-18 15:04:08 +0100

    refactor(example): valid

--- src/examples/valid/main.lua
+++ src/examples/valid/main.lua
@@ -1,24 +1,24 @@
 r = user_input()
 
-min_length = function(n)
+function min_length(n)
   return function(s)
-    if string.len(s) > n then
+    if n < string.len(s) then
       return true
     end
-    return false, 'too short!'
+    return false, "too short!"
   end
 end
 
-max_length = function(n)
+function max_length(n)
   return function(s)
     if string.len(s) < n then
       return true
     end
-    return false, 'too long!'
+    return false, "too long!"
   end
 end
 
-is_upper = function(s)
+function is_upper(s)
   local ret = true
   for i = 1, string.ulen(s) do
     local v = string.char_at(s, i)
@@ -29,22 +29,24 @@ is_upper = function(s)
   if ret then
     return true
   end
-  return false, 'should be all uppercase'
+  return false, "should be all uppercase"
 end
 
-is_number = function(s)
+function is_number(s)
   local n = tonumber(s)
-  if n then return true end
-  return false, 'NaN'
+  if n then
+    return true
+  end
+  return false, "NaN"
 end
 
 function love.update()
-  if r:is_empty() then
-    validated_input(
-      { min_length(2), is_number },
-      'Enter a number greater than 100:')
+  if not r:is_empty() then
+    validated_input({
+      min_length(2),
+      is_number
+    })
   else
-    local input = r()
-    print(input)
+    print(r())
   end
 end
