d06a91d123973cbbad0bda65e67ff89dd82c77ad
aldum <aldum@artixlinux.org>
2025-01-11 14:16:46 +0100

    refactor(example): split up turtle

---
+++ src/examples/turtle/drawing.lua
@@ -0,0 +1,81 @@
+local G = love.graphics
+
+font = G.newFont()
+bg_color = Color.black
+debugColor = Color.yellow
+
+function drawBackground(color)
+  local c = bg_color
+  local not_green = color ~= Color.green
+       and color ~= Color.green + Color.bright
+  local color_valid = Color.valid(color) and not_green
+  if color_valid then
+    c = color
+  end
+  G.setColor(Color[c])
+  G.rectangle("fill", 0, 0, width, height)
+end
+
+function drawFrontLegs(x_r, y_r, leg_xr, leg_yr)
+  G.setColor(Color[Color.green + Color.bright])
+  G.push("all")
+  G.translate(-x_r, -y_r / 2 - leg_xr)
+  G.rotate(-math.pi / 4)
+  G.ellipse("fill", 0, 0, leg_xr, leg_yr, 100)
+  G.pop()
+  G.push("all")
+  G.translate(x_r, -y_r / 2 - leg_xr)
+  G.rotate(math.pi / 4)
+  G.ellipse("fill", 0, 0, leg_xr, leg_yr, 100)
+  G.pop()
+end
+
+function drawHindLegs(x_r, y_r, leg_r, leg_yr)
+  G.setColor(Color[Color.green + Color.bright])
+  G.push("all")
+  G.translate(-x_r, y_r / 2 + leg_r)
+  G.rotate(math.pi / 4)
+  G.ellipse("fill", 0, 0, leg_r, leg_yr, 100)
+  G.pop()
+  G.push("all")
+  G.translate(x_r, y_r / 2 + leg_r)
+  G.rotate(-math.pi / 4)
+  G.ellipse("fill", 0, 0, leg_r, leg_yr, 100)
+  G.pop()
+end
+
+function drawBody(x_r, y_r, head_r)
+  --- body
+  G.setColor(Color[Color.green])
+  G.ellipse("fill", 0, 0, x_r, y_r, 100)
+  --- head
+  local neck = 5
+  G.circle("fill", 0, ((0 - y_r) - head_r) + neck, head_r, 100)
+  --- end
+end
+
+function drawTurtle(x, y)
+  local head_r = 8
+  local leg_xr = 5
+  local leg_yr = 10
+  local x_r = 15
+  local y_r = 20
+  G.push("all")
+  G.translate(x, y)
+  drawFrontLegs(x_r, y_r, leg_xr, leg_yr)
+  drawHindLegs(x_r, y_r, leg_xr, leg_yr)
+  drawBody(x_r, y_r, head_r)
+  G.pop()
+end
+function drawHelp()
+  G.setColor(Color[Color.white])
+  G.print("Press [I] to open console", 20, 20)
+  local help = "Enter 'forward', 'back', 'left', or 'right'" .. 
+      "to move the turtle!"
+  G.print(help, 20, 50)
+end
+function drawDebuginfo()
+  G.setColor(Color[debugColor])
+  local dt = string.format("Turtle position: (%d, %d)", tx, ty)
+  G.print(dt, width - 200, 20)
+end
--- src/examples/turtle/main.lua
+++ src/examples/turtle/main.lua
@@ -1,156 +1,55 @@
-local G = love.graphics
+require("action")
+require("drawing")
 
-width, height = G.getDimensions()
+width, height = love.graphics.getDimensions()
 midx = width / 2
 midy = height / 2
 incr = 10
 
 tx, ty = midx, midy
 debug = false
-debugColor = Color.yellow
-
-bg_color = Color.black
 
 local r = user_input()
-
-function drawBackground(color)
-  local c = bg_color
-  local not_green = color ~= Color.green
-      and color ~= Color.green + Color.bright
-  local color_valid = Color.valid(color) and not_green
-  if color_valid then c = color end
-
-  G.setColor(Color[c])
-  G.rectangle('fill', 0, 0, width, height)
-end
-
-local function drawFrontLegs(x_r, y_r, leg_r)
-  G.setColor(Color[Color.green + Color.bright])
-  G.push('all')
-  G.translate(-x_r, -y_r / 2 - leg_r)
-  G.rotate(-math.pi / 4)
-  G.ellipse("fill", 0, 0, leg_r, 10, 100)
-  G.pop()
-  G.push('all')
-  G.translate(x_r, -y_r / 2 - leg_r)
-  G.rotate(math.pi / 4)
-  G.ellipse("fill", 0, 0, leg_r, 10, 100)
-  G.pop()
-end
-local function drawHindLegs(x_r, y_r, leg_r)
-  G.setColor(Color[Color.green + Color.bright])
-  G.push('all')
-  G.translate(-x_r, y_r / 2 + leg_r)
-  G.rotate(math.pi / 4)
-  G.ellipse("fill", 0, 0, leg_r, 10, 100)
-  G.pop()
-  G.push('all')
-  G.translate(x_r, y_r / 2 + leg_r)
-  G.rotate(-math.pi / 4)
-  G.ellipse("fill", 0, 0, leg_r, 10, 100)
-  G.pop()
-end
-local function drawBody(x_r, y_r, head_r)
-  -- body
-  G.setColor(Color[Color.green])
-  G.ellipse("fill", 0, 0, x_r, y_r, 100)
-  -- head
-  G.circle("fill", 0, 0 - y_r - head_r + 5, head_r, 100)
-end
-
-function drawTurtle(x, y)
-  local head_r = 8
-  local leg_r = 5
-  local x_r = 15
-  local y_r = 20
-  G.push('all')
-
-  G.translate(x, y)
-  drawFrontLegs(x_r, y_r, leg_r)
-  drawHindLegs(x_r, y_r, leg_r)
-  drawBody(x_r, y_r, head_r)
-  G.pop()
-end
-
-local function drawHelp()
-  G.setColor(Color[Color.white])
-  G.print("Press [I] to open console", 20, 20)
-  G.print("Enter 'forward', 'back', 'left', or 'right' to move the turtle!", 20, 40)
-end
-
-local function drawDebuginfo()
-  G.setColor(Color[debugColor])
-  local dt = string.format("Turtle position: (%d, %d)", tx, ty)
-  G.print(dt, width - 200, 20)
-end
-
-function move_forward(d)
-  ty = ty - (d or incr)
-end
-
-function move_back(d)
-  ty = ty + (d or incr)
-end
-
-function move_left(d)
-  tx = tx - (d or (2 * incr))
-end
-
-function move_right(d)
-  tx = tx + (d or (2 * incr))
-end
-
-function pause() stop('user stop') end
-
-actions = {
-  forward = move_forward,
-  fd      = move_forward,
-  back    = move_back,
-  b       = move_back,
-  left    = move_left,
-  l       = move_left,
-  right   = move_right,
-  r       = move_right,
-  stop    = pause,
-  pause   = pause,
-}
 
 function eval()
   local input = r()
   local f = actions[input]
-
-  if f then f() end
+  if f then
+    f()
+  end
 end
 
 function love.draw()
+  G.setFont(font)
   drawBackground()
   drawHelp()
   drawTurtle(tx, ty)
-  if debug then drawDebuginfo() end
+  if debug then
+    drawDebuginfo()
+  end
 end
 
 function love.keypressed(key)
   if love.keyboard.isDown("lshift", "rshift") then
-    if key == 'r' then
+    if key == "r" then
       tx, ty = midx, midy
     end
   end
-  if key == 'space' then
+  if key == "space" then
     debug = not debug
   end
-  if key == 'pause' then
-    stop()
+  if key == "pause" then
+    pause()
   end
 end
 
 function love.keyreleased(key)
-  if key == 'i' then
-    input_text('TURTLE')
+  if key == "i" then
+    input_text("TURTLE")
   end
-  if key == 'return' then
+  if key == "return" then
     eval()
   end
-
   if love.keyboard.isDown("lctrl", "rctrl") then
     if key == "escape" then
       love.event.quit()
@@ -158,9 +57,7 @@ function love.keyreleased(key)
   end
 end
 
-local t = 0
-function love.update(dt)
-  t = t + dt
+function love.update()
   if ty > midy then
     debugColor = Color.red
   end
