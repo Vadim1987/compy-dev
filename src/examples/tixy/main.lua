6fd00d6fd49a09d78032e9426a1cca9aa24478eb
aldum <aldum@artixlinux.org>
2025-05-14 21:46:33 +0200

    feat(tixy): add examples

--- src/examples/tixy/main.lua
+++ src/examples/tixy/main.lua
@@ -1,21 +1,61 @@
---- @diagnostic disable: duplicate-set-field,lowercase-global
-cw, ch = love.graphics.getDimensions()
+local G = love.graphics
+math.randomseed(os.time())
+cw, ch = G.getDimensions()
+midx = cw / 2
 
 require('math')
+require('examples')
 
 size = 28
 spacing = 3
 offset = size + 4
 
 local colors = {
-  bg = Color[Color.black],
-  pos = Color[Color.white],
-  neg = Color[Color.red],
+  bg   = Color[Color.black],
+  pos  = Color[Color.white],
+  neg  = Color[Color.red],
+  text = Color[Color.white]
 }
 
+body = 'return -i / (count * count)'
+legend = ''
 count = 16
+ex_idx = 1
+ex_idx = 14
+
+function load_example(ex)
+  body   = ex.code
+  legend = ex.legend
+end
+
+function advance()
+  local e = examples[ex_idx]
+  load_example(e)
+  if ex_idx < #examples then ex_idx = ex_idx + 1 end
+end
+
+function pick_random(t)
+  if type(t) == "table" then
+    local n = #t
+    local r = math.random(n)
+    return t[r]
+  end
+end
+
+function randomize()
+  local e = pick_random(examples)
+  load_example(e)
+end
+
+function b2n(b)
+  if b then return 1 else return 0 end
+end
+
+function n2b(n)
+  if n ~= 0 then return true else return false end
+end
+
 local time = 0
-body = "return (x - y) - math.sin(t)"
 callback = function(t, i, x, y)
   local code = [[
     local count = ...
@@ -67,6 +107,9 @@ function love.draw()
   G.rectangle("fill", 0, 0, cw, ch)
 
   drawOutput()
+  G.setColor(colors.text)
+  local sof = (size / 2) + offset
+  G.printf(legend, midx + sof, sof, midx - sof)
 end
 
 r = user_input()
@@ -74,9 +117,12 @@ r = user_input()
 function love.update(dt)
   time = time + dt
   if r:is_empty() then
-    input_code('function tixy(t, i, x, y)')
+    input_code('function tixy(t, i, x, y)', string.lines(body))
   else
     local ret = r()
     body = string.unlines(ret)
+    legend = ''
   end
 end
+
+advance()
