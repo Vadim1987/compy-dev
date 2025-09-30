a0fc77356aca7a13e5fd4a4d8e327c17582c393a
Vadim1987 <vadim.stetskiy@gmail.com>
2025-09-30 13:33:07 +0200

    2048-1-iteration(refactored 2)

--- src/examples/2048-1-version/main.lua
+++ src/examples/2048-1-version/main.lua
@@ -10,8 +10,22 @@ require("board")
 board = Board.new(4, 4)
 
 -- Keyboard input: arrows/WASD, R restart, Esc quit
-
-edi()
+function love.keypressed(key)
+  local map = {
+    left="left", right="right", up="up", down="down",
+    a="left", d="right", w="up", s="down",
+  }
+  if key == "escape" and love and love.event and love.event.quit then
+    love.event.quit()
+    return
+  end
+  local dir = map[key]
+  if dir then
+    if board:move(dir) then board:add_random_tile() end
+  elseif key == "r" then
+    board:seed()
+  end
+end
 
 -- Draw board and a tiny HUD
 function love.draw()
